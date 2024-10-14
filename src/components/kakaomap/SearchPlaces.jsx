38adb9b78aebe92a45281b2dae0ee98c18bdf6e7
고건영 <gun5297@naver.com>
2024-10-14 11:23:51 +0900

    css 살짝 수정

--- src/components/kakaomap/SearchPlaces.jsx
+++ src/components/kakaomap/SearchPlaces.jsx
@@ -13,13 +13,11 @@ const SearchPlaces = () => {
 
     return (
         <SearchPlacesWrap>
-            <InnerWrap>
-                <Button onClick={() => handleSearch('인천 남동구 선수촌공원로 37 중앙프라자')}>
-                    중앙공인중개사
-                </Button>
-                <Button onClick={() => handleSearch('구월동 블루시티 오피스텔')}>블루시티</Button>
-                <Button onClick={() => handleSearch('구월동 두플라스')}>두플러스</Button>
-            </InnerWrap>
+            <Button onClick={() => handleSearch('인천 남동구 선수촌공원로 37 중앙프라자')}>
+                중앙공인중개사
+            </Button>
+            <Button onClick={() => handleSearch('구월동 블루시티 오피스텔')}>블루시티</Button>
+            <Button onClick={() => handleSearch('구월동 두플라스')}>두플러스</Button>
         </SearchPlacesWrap>
     );
 };
--- src/components/kakaomap/styled.js
+++ src/components/kakaomap/styled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const SearchPlacesWrap = styled.div`
+    margin: auto;
     button {
         width: 33%;
         height: 60px;
